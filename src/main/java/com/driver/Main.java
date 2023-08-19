a4ea95092628938324ebcdefd820c55f4149612d
Aniket Birambole <aniketb1811@gmail.com>
2023-08-19 13:54:57 +0530

    Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,19 @@
 package com.driver;
 
 public class Main{
+
+  public static class A{
+    public String meth(){
+      return "Invoking method from class A";
+    }
+  }
+
+  public static class B extends A{
+    @Override
+    public String meth(){
+      return "Invoking method from class A";
+    }
+  }
   public static void main(String []args){
     A b = new B();
     b.meth();
